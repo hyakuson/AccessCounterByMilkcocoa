09e76ab0346dd09e70d52304fe066179bf9c77e7
Ryohei Hyakusoku <ryoheihyakusan@gmail.com>
2015-12-19 22:59:01 +0900

    jsを読み込むだけでアクセスカウントするように変更

--- accesscounter.js
+++ accesscounter.js
@@ -1,74 +1,67 @@
 (function () {
     'use strict';
-    window.accesscounter = {
 
-        start: function (site, page) {
-            if (!site) {
-                site = location.hostname || "localhost";
-            }
-            if (!page) {
-                page = location.pathname;
-            }
+    var mk,
+        dsSite, // サイト全体のアクセス数用
+        dsPage, // ページ単位のアクセス数用
+        now = new Date(),
+        strToday = now.getFullYear() + "/" + ("0" + (now.getMonth() + 1)).slice(-2) + "/" + ("0" + now.getDate()).slice(-2),
 
-            var mk,
-                dsSite, // サイト全体のアクセス数用
-                dsPage, // ページ単位のアクセス数用
-                now = new Date(),
-                strToday = now.getFullYear() + "/" + ("0" + (now.getMonth() + 1)).slice(-2) + "/" + ("0" + now.getDate()).slice(-2),
+        site = location.hostname || "localhost",
+        page = location.pathname,
 
-                todayCountId = strToday,
-                totalCountId = "TotalCount_Page",
-                siteTotalCountId = "TotalCount_Site",
-                siteDayTotalCountId = strToday;
+        todayCountId = strToday,
+        totalCountId = "TotalCount_Page",
+        siteTotalCountId = "TotalCount_Site",
+        siteDayTotalCountId = strToday;
 
-            function accessCount(id, pageOrSite) {
-                var mk = new window.MilkCocoa("onicovyhpw.mlkcca.com"),
-                    ds;
+    function accessCount(id, pageOrSite) {
+        var mk = new window.MilkCocoa("onicovyhpw.mlkcca.com"),
+            ds;
 
-                if (pageOrSite === "page") {
-                    ds = mk.dataStore(site).child(page);
-                } else if (pageOrSite === "site") {
-                    ds = mk.dataStore(site);
-                }
+        if (pageOrSite === "page") {
+            ds = mk.dataStore(site).child(page);
+        } else if (pageOrSite === "site") {
+            ds = mk.dataStore(site);
+        }
 
-                ds.get(id, function (err, datum) {
-                    if (err) {
-                        // 初回はデータがなくてエラーになるので初期データをセットする
-                        if (err === "not found") {
-                            ds.set(id, {
-                                "count": "1"
-                            }, function (err, datum) {
-                                mk.disconnect();
-                            });
-                        } else {
-                            window.console.log(err);
-                        }
-                        return;
-                    }
+        ds.get(id, function (err, datum) {
+            if (err) {
+                // 初回はデータがなくてエラーになるので初期データをセットする
+                if (err === "not found") {
                     ds.set(id, {
-                        "count": (Number(datum.value.count) + 1).toString()
+                        "count": "1"
                     }, function (err, datum) {
                         mk.disconnect();
                     });
-                });
+                } else {
+                    window.console.log(err);
+                }
+                return;
             }
+            ds.set(id, {
+                "count": (Number(datum.value.count) + 1).toString()
+            }, function (err, datum) {
+                mk.disconnect();
+            });
+        });
+    }
 
-            /*** ページごとのアクセス数 ***/
+    /*** ページごとのアクセス数 ***/
 
-            // 日ごとのページへのアクセス数をカウント
-            accessCount(todayCountId, "page");
+    // 日ごとのページへのアクセス数をカウント
+    accessCount(todayCountId, "page");
 
-            // トータルアクセス数をカウント
-            accessCount(totalCountId, "page");
+    // トータルアクセス数をカウント
+    accessCount(totalCountId, "page");
 
 
-            /***サイト全体のカウント***/
+    /***サイト全体のカウント***/
 
-            // 日ごとのサイト全体へのアクセス数をカウント
-            accessCount(siteDayTotalCountId, "site");
+    // 日ごとのサイト全体へのアクセス数をカウント
+    accessCount(siteDayTotalCountId, "site");
 
-            // 全日のサイト全体へのアクセス数をカウント
-            accessCount(siteTotalCountId, "site");
-        }
-    };
+    // 全日のサイト全体へのアクセス数をカウント
+    accessCount(siteTotalCountId, "site");
+
 }());
